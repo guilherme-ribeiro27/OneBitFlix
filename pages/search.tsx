34bb48309c83fabf7e716c33886943bd959c3623
guilherme-ribeiro27 <gui062702@gmail.com>
2023-01-08 10:11:00 -0300

    Search page styled

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,6 +4,9 @@ import HeaderAuth from '../src/components/common/headerAuth'
 import styles from '../styles/search.module.scss'
 import { useRouter } from 'next/router'
 import courseService, { CourseType } from '../src/services/courseService'
+import { Container } from 'reactstrap'
+import { SearchCard } from '../src/components/searchCard'
+import Footer from '../src/components/common/footer'
 
 const Search = () => {
     const router = useRouter()
@@ -27,13 +30,24 @@ const Search = () => {
             <title>Onebitflix - {searchName}</title>
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         </Head>
-        <main>
-            <HeaderAuth/>
-            {searchResults?.map((course)=>(
-                <div key={course.id}>
-                    <h1>{course.name}</h1>
+        <main className={styles.main}>
+            <div className={styles.headerFooterBg}>
+                <HeaderAuth/>
+            </div>
+            {searchResults.length >= 1 ? (
+                <div className={styles.searchResult}>
+                    <Container className='d-flex flex-wrap justify-content-center gap-5 py-4'>
+                    {searchResults?.map((course)=>(
+                        <SearchCard key={course.id} course={course}/>
+                    ))}
+                </Container>
                 </div>
-            ))}
+            ) : (
+                <p className={styles.noSearchResult}>Nenhum resultado encontrado</p>
+            )}
+            <div className={styles.headerFooterBg}>
+                <Footer/>
+            </div>
         </main>
     </>
 )
---
+++ src/components/searchCard/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import { CourseType } from '../../services/courseService'
+import Link from 'next/link'
+
+interface props{
+    course: CourseType
+}
+
+export const SearchCard = ({course}:props) => {
+  return (
+    <Link href={`/courses/${course.id}`}>
+        <div className={styles.searchCard}>
+            <img src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} alt={course.name} className={styles.searchCardImg}/>
+            <p className={styles.searchCardName}>{course.name}</p>
+            <p className={styles.searchCardDescription}>{course.synopsis}</p>
+        </div>
+    </Link>
+  )
+}
